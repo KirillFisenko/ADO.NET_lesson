47ea0f88142abd2b3f8aa11b34432b055dbc1afe
KirillFisenko <79265463469@ya.ru>
2024-10-24 15:07:54 +0300

    Add project files.

---
+++ ADO.NET_lesson/Program.cs
@@ -0,0 +1,24 @@
+﻿using MySql.Data.MySqlClient;
+
+public class Program
+{
+    public static void Main()
+    {
+        string connectionString = "Server=localhost;Database=test;Uid=root;Pwd=m48kHz16bit%;";
+
+        using (var connection = new MySqlConnection(connectionString))
+        {
+            connection.Open();
+            Console.WriteLine("Подключение открыто");
+
+            // Вывод информации о подключении
+            Console.WriteLine(@$"Свойства подключения:
+            Строка подключения: {connection.ConnectionString}
+            База данных: {connection.Database}
+            Сервер: {connection.DataSource}
+            Версия сервера: {connection.ServerVersion}
+            Состояние: {connection.State}");
+        }
+        Console.WriteLine("Подключение закрыто");
+    }
+}
