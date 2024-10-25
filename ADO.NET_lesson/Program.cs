d10eb260a5e694cd7e47b057063200d306e63bb6
KirillFisenko <79265463469@ya.ru>
2024-10-25 20:08:21 +0300

    К уроку 3.1 Управление БД https://stepik.org/lesson/1364141/step/1?unit=1380052

--- ADO.NET_lesson/Program.cs
+++ ADO.NET_lesson/Program.cs
@@ -1,24 +1,41 @@
 ﻿using MySql.Data.MySqlClient;
+using System.Reflection;
 
 public class Program
 {
     public static void Main()
     {
-        string connectionString = "Server=localhost;Database=test;Uid=root;Pwd=m48kHz16bit%;";
+        // Строка подключения к базе данных MySQL
+        string connectionString = "Server=localhost;Database=test;Uid=root;Pwd=;";
 
+        // Использование блока using для автоматического закрытия соединения
         using (var connection = new MySqlConnection(connectionString))
         {
+            // Открытие соединения
             connection.Open();
             Console.WriteLine("Подключение открыто");
 
             // Вывод информации о подключении
-            Console.WriteLine(@$"Свойства подключения:
-            Строка подключения: {connection.ConnectionString}
-            База данных: {connection.Database}
-            Сервер: {connection.DataSource}
-            Версия сервера: {connection.ServerVersion}
-            Состояние: {connection.State}");
+            PrintProperties(connection);
         }
         Console.WriteLine("Подключение закрыто");
+    }
+
+    // Метод для вывода всех свойств объекта
+    private static void PrintProperties(object obj)
+    {
+        // Получение типа объекта
+        Type type = obj.GetType();
+        // Получение всех свойств объекта
+        PropertyInfo[] properties = type.GetProperties();
+
+        // Перебор всех свойств
+        foreach (var property in properties)
+        {
+            // Получение значения свойства
+            object value = property.GetValue(obj, null);
+            // Вывод имени и значения свойства
+            Console.WriteLine($"{property.Name}: {value}");
+        }
     }
 }
