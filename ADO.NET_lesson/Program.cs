05b58b5ec3439f7a5230319f1ff24f3ccfac0c8b
KirillFisenko <79265463469@ya.ru>
2024-11-14 21:54:13 +0300

    К уроку 3.3 Выполнение SQL запросов часть 2 https://stepik.org/lesson/1364143/step/3?unit=1380054

--- ADO.NET_lesson/Program.cs
+++ ADO.NET_lesson/Program.cs
@@ -1,4 +1,5 @@
 ﻿using MySql.Data.MySqlClient;
+using System.Data;
 
 public class Program
 {
@@ -40,49 +41,105 @@ public class Program
     //    var execute = command.ExecuteNonQuery();
     //}
 
+    //public static void Main()
+    //{
+    //    var firstName = "Василий";
+    //    var lastName = "Петров";
+    //    var email = "1email@mail.com";
+    //    var age = 30;
+    //    var connectionString = "Server=localhost;Database=test;Uid=root;Pwd=m48kHz16bit%;";
+    //    using var connection = new MySqlConnection(connectionString);
+    //    connection.Open();
+    //    var sqlQuery = $@"INSERT INTO users (first_name, last_name, email, age) VALUES
+    //                                         (@firstName, @lastName, @email, @age);";
+    //    using var command = new MySqlCommand(sqlQuery, connection);
+
+    //    // Создание параметра для имени
+    //    var firstNameParam = new MySqlParameter
+    //    {
+    //        // Установка имени параметра
+    //        ParameterName = "@firstName",
+
+    //        // Установка значения параметра
+    //        Value = firstName,
+
+    //        // Установка типа данных параметра
+    //        MySqlDbType = MySqlDbType.VarChar,
+
+    //        // Установка размера параметра (в данном случае длина строки)
+    //        Size = 100,
+
+    //        // Установка флага, указывающего, может ли параметр быть NULL
+    //        IsNullable = false
+    //    };
+
+    //    var lastNameParam = new MySqlParameter("@lastName", lastName);
+    //    var emailParam = new MySqlParameter("@email", email);
+    //    var ageParam = new MySqlParameter("@age", age);
+
+    //    // Добавляем параметры для запроса
+    //    command.Parameters.Add(firstNameParam);
+    //    command.Parameters.Add(lastNameParam);
+    //    command.Parameters.Add(emailParam);
+    //    command.Parameters.Add(ageParam);
+
+    //    // Выполнение запроса
+    //    var execute = command.ExecuteNonQuery();
+    //}
+
     public static void Main()
     {
         var firstName = "Василий";
         var lastName = "Петров";
-        var email = "1email@mail.com";
+        var email = "email@email.com";
         var age = 30;
-        var connectionString = "Server=localhost;Database=test;Uid=root;Pwd=m48kHz16bit%;";
+        var connectionString = "Server=localhost;Database=test;Uid=root;Pwd=;";
         using var connection = new MySqlConnection(connectionString);
         connection.Open();
-        var sqlQuery = $@"INSERT INTO users (first_name, last_name, email, age) VALUES
-                                             (@firstName, @lastName, @email, @age);";
-        using var command = new MySqlCommand(sqlQuery, connection);
 
-        // Создание параметра для имени
-        var firstNameParam = new MySqlParameter
+        // Создание команды для вызова хранимой процедуры
+        var storedProcedureName = "p_InsertUser";
+        using var command = new MySqlCommand(storedProcedureName, connection);
+        command.CommandType = CommandType.StoredProcedure;
+
+        // Указываем входные параметры
+        var firstNameParam = new MySqlParameter("p_firstName", firstName)
         {
-            // Установка имени параметра
-            ParameterName = "@firstName",
-
-            // Установка значения параметра
-            Value = firstName,
-
-            // Установка типа данных параметра
-            MySqlDbType = MySqlDbType.VarChar,
-
-            // Установка размера параметра (в данном случае длина строки)
-            Size = 100,
-
-            // Установка флага, указывающего, может ли параметр быть NULL
-            IsNullable = false
+            Direction = ParameterDirection.Input
+        };
+        var lastNameParam = new MySqlParameter("p_lastName", lastName)
+        {
+            Direction = ParameterDirection.Input
+        };
+        var emailParam = new MySqlParameter("p_email", email)
+        {
+            Direction = ParameterDirection.Input
+        };
+        var ageParam = new MySqlParameter("p_age", age)
+        {
+            Direction = ParameterDirection.Input
         };
 
-        var lastNameParam = new MySqlParameter("@lastName", lastName);
-        var emailParam = new MySqlParameter("@email", email);
-        var ageParam = new MySqlParameter("@age", age);
+        // Создание выходного параметра для идентификатора пользователя
+        var userIdParam = new MySqlParameter("p_userId", MySqlDbType.Int32)
+        {
+            Direction = ParameterDirection.Output
+        };
 
         // Добавляем параметры для запроса
         command.Parameters.Add(firstNameParam);
         command.Parameters.Add(lastNameParam);
         command.Parameters.Add(emailParam);
         command.Parameters.Add(ageParam);
+        command.Parameters.Add(userIdParam);
 
-        // Выполнение запроса
-        var execute = command.ExecuteNonQuery();
+        // Выполнение команды
+        command.ExecuteNonQuery();
+
+        // Получение значения выходного параметра
+        var userId = userIdParam.Value;
+
+        // Вывод идентификатора вставленного пользователя
+        Console.WriteLine("Inserted User ID: " + userId);
     }
 }
