07493250c898b53cb34cb4fe23978bd5b7bab977
KirillFisenko <79265463469@ya.ru>
2025-01-12 17:20:47 +0300

    К уроку 3.5 DataSet https://stepik.org/lesson/1480914/step/1?unit=1500628

--- ADO.NET_lesson/Program.cs
+++ ADO.NET_lesson/Program.cs
@@ -1,63 +1,87 @@
 ﻿using MySql.Data.MySqlClient;
-using System.Reflection;
+using System.Data;
 
 public class Program
 {
-    public static void Main()
+    public static void Main1()
     {
         // Строка подключения к базе данных MySQL
-        string connectionString = "Server=localhost;Database=test;Uid=root;Pwd=;";
+        string connectionString = "Server=localhost;Database=test;Uid=root;Pwd=";
 
         // Создание подключения с автоматическим закрытием соединения
-        using (var connection = new MySqlConnection(connectionString))
+        using var connection = new MySqlConnection(connectionString);
+
+        // Открытие соединения
+        connection.Open();
+
+        // Составление SQL-выражения для чтения данных из таблицы
+        string sqlQuery = "SELECT * FROM USERS;";
+
+        // Создание объекта для инкапсуляции выполняемого SQL-выражения 
+        using MySqlCommand command = new MySqlCommand(sqlQuery, connection);
+
+        // Создание объекта, который используется для чтения данных
+        using MySqlDataReader reader = command.ExecuteReader();
+
+        // Чтение строк из набора данных
+        while (reader.Read())
         {
-            // Открытие соединения
-            connection.Open();
+            // Получение значений столбцов и их форматирование для ровного вывода
+            var value_id = reader["id"].ToString().PadRight(10);
+            var value_firstName = reader["first_name"].ToString().PadRight(15);
+            var value_lastName = reader["last_name"].ToString().PadRight(15);
+            var value_email = reader["email"].ToString().PadRight(26);
+            var value_age = reader["age"].ToString().PadRight(8);
+            var value_createdAt = reader["created_at"].ToString().PadRight(20);
 
-            // Составление SQL-выражения на создание таблицы
-            string sqlQuery = @"CREATE TABLE IF NOT EXISTS users (
-                                  id INT AUTO_INCREMENT PRIMARY KEY,
-                                  first_name VARCHAR(50) NOT NULL,
-                                  last_name VARCHAR(50) NOT NULL,
-                                  email VARCHAR(100) UNIQUE NOT NULL,
-                                  age INT,
-                                  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-                               );";
-
-            // Создание объекта для инкапсуляции выполняемого SQL-выражения 
-            MySqlCommand command = new MySqlCommand(sqlQuery, connection);
-
-            // Выполнение команды на создание таблицы
-            var execute = command.ExecuteNonQuery();
-
-            Console.WriteLine($"Создана таблица users. Количество измененных записей: {execute}");
-
-            // Переопределяем SQL-выражение, вставляем данные в таблицу
-            command.CommandText = @"INSERT INTO users (first_name, last_name, email, age) VALUES
-                                    ('John', 'Doe', 'john.doe@example.com', 30),
-                                    ('Jane', 'Smith', 'jane.smith@example.com', 25),
-                                    ('Alice', 'Johnson', 'alice.johnson@example.com', 28),
-                                    ('Bob', 'Brown', 'bob.brown@example.com', 35),
-                                    ('Charlie', 'Davis', 'charlie.davis@example.com', 22);
-                                   ";
-
-            // Выполнение команды на вставку данных
-            execute = command.ExecuteNonQuery();
-
-            Console.WriteLine($"Количество вставленных записей в таблицу users: {execute}");
+            // Вывод значений столбцов
+            Console.WriteLine($"{value_id} {value_firstName} {value_lastName} {value_email} {value_age} {value_createdAt}");
         }
     }
 
-    // Метод для вывода всех свойств объекта
-    private static void PrintProperties(object obj)
+    public static void Main2()
     {
-        Type type = obj.GetType();
-        PropertyInfo[] properties = type.GetProperties();
+        // Строка подключения к базе данных MySQL
+        string connectionString = "Server=localhost;Database=test;Uid=root;Pwd=m48kHz16bit%;";
 
-        foreach (var property in properties)
+        // Создание подключения с автоматическим закрытием соединения
+        using var connection = new MySqlConnection(connectionString);
+
+        // Составление SQL-выражения для чтения данных из таблицы
+        string sqlQuery = "SELECT * FROM USERS;";
+
+        // Создание SqlDataAdapter для заполнения DataSet
+        var dataAdapter = new MySqlDataAdapter(sqlQuery, connection);
+
+        // Создание объекта DataSet, который хранит данные в памяти в виде таблиц
+        var dataSet = new DataSet();
+
+        // Заполнение DataSet данными
+        dataAdapter.Fill(dataSet);
+
+        // Зададим отступ для колонок
+        var indent = 14;
+
+        // Перебор таблиц
+        foreach (DataTable table in dataSet.Tables)
         {
-            object value = property.GetValue(obj, null);
-            Console.WriteLine($"{property.Name}: {value}");
+            // Перебор заголовков столбцов
+            foreach (DataColumn column in table.Columns)
+            {
+                Console.Write(column.ColumnName.PadRight(indent));
+            }
+            Console.WriteLine();
+
+            // Перебор строк
+            foreach (DataRow row in table.Rows)
+            {
+                // Вывод значений ячеек
+                foreach (var item in row.ItemArray)
+                {
+                    Console.Write(item.ToString().PadRight(indent));
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
